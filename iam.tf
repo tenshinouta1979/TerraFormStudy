241730f2b7fadb89498f7c6fa4d97d1a75845167
RYAN LEE <ryan_lee@live.com>
2024-07-24 12:05:48 -0600

    added lambda function

---
+++ iam.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_role" "example" {
+  name = "example_lambda_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "example" {
+  role       = aws_iam_role.example.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = "us-west-2"
 }
 
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "function.zip"
+  output_path = "function.zip"
+}
+
 resource "aws_s3_bucket" "reference_name" {
   bucket = "tf-bucket-name"
 
@@ -10,6 +16,19 @@ resource "aws_s3_bucket" "reference_name" {
     Environment = "Dev"
   }
 }
+
+resource "aws_lambda_function" "example" {
+  function_name = "tf_example_lambda"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  handler = "index.handler"
+  runtime = "nodejs18.x"
+
+  role = aws_iam_role.example.arn
+}
+
 
 //terraform init
 //terraform apply
